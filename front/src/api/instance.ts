d6ade0fca864327c019f8696d81b3cca15438333
shaoerkuai <xujiaze22@aliyun.com>
2024-04-14 15:25:22 +0800

    feat: api

--- front/src/api/instance.ts
+++ front/src/api/instance.ts
@@ -1,16 +1,17 @@
 import axios, { InternalAxiosRequestConfig } from 'axios';
 import router from '../routes/router.ts';
-
+import { useSessionStore } from '../store/sessionStore.ts';
 export const instance = axios.create({
   baseURL: import.meta.env.VITE_APP_URL,
   timeout: 2000,
 });
-
 instance.interceptors.request.use(
   function (config: InternalAxiosRequestConfig) {
     const token = localStorage.getItem('token');
+    const store = useSessionStore();
     if (token === null) {
       (window as any).$message.error('还未登录，请重试');
+      store.clearState();
       router.push({ name: 'login' });
     } else {
       config.headers.set('Authorization', 'Bearer ' + token);
@@ -22,7 +23,6 @@ instance.interceptors.request.use(
     return Promise.reject(error);
   },
 );
-
 instance.interceptors.response.use(
   (response) => {
     if (response.status === 200) {
@@ -32,10 +32,19 @@ instance.interceptors.response.use(
     }
   },
   (error) => {
+    if (!error.response) {
+      (window as any).$message.error(error.message);
+      return Promise.reject(error);
+    }
     if (error.response.status) {
+      const store = useSessionStore();
+      console.log(error);
       switch (error.response.status) {
         case 401:
-          (window as any).$message.warning('登录已过期，请重新登录');
+          (window as any).$message.warning('登录已过期，请重试');
+          store.clearState();
+          localStorage.removeItem('token');
+          router.push({ name: 'login' });
           break;
         default:
           (window as any).$message.error(error.response);
--- front/src/views/steps/step1.vue
+++ front/src/views/steps/step1.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts">
 import { stepEvent } from '../../utils/mitt.ts';
+import { getCurrentStep } from '../../api/taskAPI.ts';
 
-const testFun = ()=>{
+const testFun = () => {
   stepEvent.emit('popStep', {
     prevStep: 1,
     prevStepStatus: 'wait',
     targetStepStatus: 'process',
   });
-}
-const testFu2 = ()=>{
+};
+const testFu2 = () => {
   stepEvent.emit('pushStep', {
     prevStep: 1,
     prevStepStatus: 'finish',
     targetStepStatus: 'process',
   });
-}
+};
+const testFunc3 = () => {
+  getCurrentStep('111').then((v) => {
+    console.log(v);
+  });
+};
 </script>
 
 <template>
@@ -22,8 +28,7 @@ const testFu2 = ()=>{
     step1
     <n-button @click="testFun">pre</n-button>
     <n-button @click="testFu2">next</n-button>
+    <n-button @click="testFunc3">req</n-button>
   </div>
 </template>
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
