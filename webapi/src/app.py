65968f9a908ce670abe5d265721a65f84bd98a40
shaoerkuai <xujiaze22@aliyun.com>
2024-04-14 22:34:52 +0800

    feat: sqlalchemy

--- webapi/src/app.py
+++ webapi/src/app.py
@@ -1,11 +1,20 @@
+from contextvars import ContextVar
+
 from redis import asyncio as aioredis
 from sanic import Sanic, json
 from sanic_jwt import initialize, scoped
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import Session
 
 from src.auth import authenticate, MyResponses, my_scope_extender, MyCustomClaim
 from src.modules.api_group_v1 import api_group_all
 
 _redis_session = aioredis.Redis(host='localhost', port=3278, db=0)
+bind = create_async_engine("mysql+aiomysql://root:123456@localhost/test", echo=True)
+_sessionmaker = async_sessionmaker(bind, expire_on_commit=False)
+
+_base_model_session_ctx = ContextVar("session")
 
 secret = 'a67636b3970539ef97727d3baf9de38618f7b1123bbda7d7f0f1353b3eb75cfbbed4210ee2f9487872132a9b4b821f993616e2cb2339fc9080f0507fcf2c0b5048254220f90c344fa2c15b2e8cf82142ac8e9604f545e7bc0be5460049d8cbac976ad174ba3aff51a1f3f5aec8819855129b2146c2638a56fecfb5d68415f4a4e1f005a0752bd8616805a34e0946aeb0a39d9ab37be0866e2045ebcb532d55e44fd12cae6d2a392785b705d007344c3ff640730808dc5c7a023140caff87cb60cd73d6cd34b8f80b0b89ba7b088b22859f82f77dd240a736c31e689daaa0740cc586dfa3ac9af3f6410088f1090ba40cdee145aa10d51377660c6ca1df5d7da3'
 
@@ -26,11 +35,24 @@ app.blueprint(api_group_all)
 @app.middleware("request")
 async def inject_redis_session(request):
     request.ctx.r_session = _redis_session
+    request.ctx.session = _sessionmaker()
+    request.ctx.session_ctx_token = _base_model_session_ctx.set(request.ctx.session)
+
+
+@app.middleware("response")
+async def close_session(request, response):
+    if hasattr(request.ctx, "session_ctx_token"):
+        _base_model_session_ctx.reset(request.ctx.session_ctx_token)
+        await request.ctx.session.close()
 
 
 @app.post('/api/v1/ds')
 @scoped(['admin', 'user'])
 async def ds(request):
+    session: AsyncSession = request.ctx.session
+    async with session.begin():
+        statement = text("""INSERT INTO test_table_3 VALUES (1111111,'2233')""")
+        await session.execute(statement)
     return json({"dd": "aa"})
 
 
